75e546fcd8e7b31da02a31102449661063c78854
Sanika Patil <patilsanika1912@gmail.com>
2026-09-01 15:23:03 +0530

    solution for maximum product of two elements

--- Arrays/Maximum-product-of-two-elements-in-array/Solution.java
+++ Arrays/Maximum-product-of-two-elements-in-array/Solution.java
@@ -1,1 +1,22 @@
-
+class Solution
+{
+  public int maxProducts(int[] nums)
+  {
+    int largest=0;
+    int secondLargest=0;
+    for(int i=0;i<nums.length;i++)
+      {
+        if(nums[i]>largest)
+        {
+          secondLargest=largest;
+          largest=nums[i];
+        }
+        else if(nums[i]>secondLargest)
+        {
+          secondLargest=nums[i];
+        }
+      }
+    int result=(largest-1)*(secondLargest-1);
+    return result;
+  }
+}
