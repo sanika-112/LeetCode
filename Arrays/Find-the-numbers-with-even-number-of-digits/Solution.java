ef4ab6f274946c7107d457ea990a2b94c6373114
Sanika Patil <patilsanika1912@gmail.com>
2026-09-02 15:45:02 +0530

    Solution for find the numbers with even number of digits

--- Arrays/Find-the-numbers-with-even-number-of-digits/Solution.java
+++ Arrays/Find-the-numbers-with-even-number-of-digits/Solution.java
@@ -1,1 +1,30 @@
-
+class Solution {
+    public boolean containsEvenDigits(int n)
+    {
+        int digits=0;
+        while(n>0)
+        {
+            digits++;
+            n=n/10;
+        }
+        if(digits%2==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(containsEvenDigits(nums[i]))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
